bfc727ec338fcc0c44f667c698a7af6c387ee899
Matt Howard <matthoward30740@gmail.com>
2022-06-10 18:48:55 -0400

    Add project files.

---
+++ WidenAPIDownload/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WidenAPIDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filenameTextBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // filenameTextBox
+            // 
+            this.filenameTextBox.Location = new System.Drawing.Point(13, 13);
+            this.filenameTextBox.Name = "filenameTextBox";
+            this.filenameTextBox.Size = new System.Drawing.Size(245, 20);
+            this.filenameTextBox.TabIndex = 0;
+            this.filenameTextBox.Text = "HWNBY NEWBURY 717.zip";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(264, 12);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(29, 23);
+            this.downloadButton.TabIndex = 1;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.AcceptsReturn = true;
+            this.urlTextBox.Location = new System.Drawing.Point(13, 40);
+            this.urlTextBox.Multiline = true;
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.ReadOnly = true;
+            this.urlTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.urlTextBox.Size = new System.Drawing.Size(339, 185);
+            this.urlTextBox.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 231);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "JSON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.filenameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filenameTextBox;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WidenAPIDownload/Form1.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WidenAPIDownload
+{
+    public partial class Form1 : Form
+    {
+        WebClient webClient;
+        Stopwatch sw = new Stopwatch();
+        string downloadPath = @"C:\Temp\ShawAPI\Download\";
+        string tempPath = @"C:\Temp\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Directory.CreateDirectory(downloadPath);
+            Directory.CreateDirectory(tempPath);
+        }
+        private void DownloadFile(string urlAddress, string filename)
+        {
+            using (webClient = new WebClient())
+            {
+                string httpType = "https://";
+                
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                // The variable that will be holding the url address (making sure it starts with http://)
+                Uri URL = urlAddress.StartsWith(httpType, StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri(httpType + urlAddress);
+
+                // Start the stopwatch which we will be using to calculate the download speed
+                sw.Start();
+
+                try
+                {
+                    // Start downloading the file
+                    webClient.DownloadFileAsync(URL, tempPath + filename + "." + ".zip");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Calculate download speed and output it to labelSpeed.
+            //labelSpeed.Text = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+
+            // Update the progressbar percentage only when the value is not the same.
+            progressBar1.Value = e.ProgressPercentage;
+
+            // Show the percentage on our label.
+            //labelPerc.Text = e.ProgressPercentage.ToString() + "%";
+
+            // Update the label with how much data have been downloaded so far and the total size of the file we are currently downloading
+            /*labelDownloaded.Text = string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));*/
+        }
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Reset the stopwatch.
+            sw.Reset();
+
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Download has been canceled.");
+            }
+            else
+            {
+                //Process(downloadName);
+                MessageBox.Show("Download completed!");
+            }
+        }
+                
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            string filename = filenameTextBox.Text;
+            string userName = "matt.howard@brownind.com";
+            string userPassword = "Brown2020";
+            string url = "https://shawfloors.widencollective.com/api/rest/asset/search/" + filename + ".zip?options=downloadUrl&metadata=roomsceneCX52colorcorrect";
+            const string widenUrl = "https://shawfloors.widencollective.com/login.loginform";
+            string getJSON = "";
+
+
+
+            // validate your login! 
+
+
+            //string getJSON = client.DownloadString(url);
+
+            JObject o1 = JObject.Parse(File.ReadAllText(getJSON));
+
+            string downloadUrl = (string)o1["assets"][0]["downloadUrl"];
+            string cc = (string)o1["assets"][0]["metadata"][0]["value"];
+            string results = (string)o1["numResults"];
+            if (cc == null)
+            {
+                cc = "No";
+            }
+
+            urlTextBox.Text += downloadUrl + "\r\n";
+            urlTextBox.Text += cc + "\r\n";
+            urlTextBox.Text += results + "\r\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string jsonString = @"C:\Users\mhoward\Desktop\Brown Automation Testing\0736V Endura 512C Plus J96 00059994 48.zip.json";
+
+            JObject o1 = JObject.Parse(File.ReadAllText(jsonString));
+
+            string downloadUrl = (string)o1["assets"][0]["downloadUrl"];
+            string cc = (string)o1["assets"][0]["metadata"][0]["value"];
+            string results = (string)o1["numResults"];
+            if (cc == null)
+            {
+                cc = "No";
+            }
+            urlTextBox.Text += "Download Url: " + downloadUrl + "\r\n\r\n";
+            urlTextBox.Text += "Is it CC: " + cc + "\r\n\r\n";
+            urlTextBox.Text += "How Many Results: " + results + "\r\n\r\n";
+        }
+    }    
+}
+
+
+
